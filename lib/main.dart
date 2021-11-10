8fa5ebab64f4c331b195ab1d0dd7cce675b4c1fa
Danny Sun <danny.sun@grabtaxi.com>
2021-11-10 14:45:57 +0800

    Simple Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: CounterPage(),
-    );
-  }
+  runApp(const CounterPage());
 }
 
 class CounterPage extends StatefulWidget {
   const CounterPage({Key? key}) : super(key: key);
-
 
   @override
   State<CounterPage> createState() => _CounterPageState();
@@ -29,21 +16,23 @@ class _CounterPageState extends State<CounterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          '$_counter',
-          style: Theme.of(context).textTheme.headline1,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
+    return MaterialApp(
+        title: "Simple Counter",
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                _counter++;
+              });
+            },
+            child: const Icon(Icons.add),
+          ),
+        ));
   }
 }
