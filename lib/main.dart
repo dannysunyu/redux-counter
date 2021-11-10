969550055368d73d09d5eab39ce62b1a936b8c09
Danny Sun <danny.sun@grabtaxi.com>
2021-11-10 14:48:40 +0800

    Provider Counter

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => Counter(),
-      child: const MyApp(),
+      child: const CounterPage(),
     ),
   );
 }
@@ -22,39 +22,29 @@ class Counter extends ChangeNotifier {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: CounterPage(),
-    );
-  }
-}
-
 class CounterPage extends StatelessWidget {
   const CounterPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Consumer<Counter>(builder: (context, counter, child) {
-          return Text(
-            '${counter.count}',
-            style: Theme.of(context).textTheme.headline1,
-          );
-        }),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          final counter = Provider.of<Counter>(context, listen: false);
-          counter.increment();
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
+    return MaterialApp(
+        title: "Provider Counter",
+        home: Scaffold(
+          body: Center(
+            child: Consumer<Counter>(builder: (context, counter, child) {
+              return Text(
+                '${counter.count}',
+                style: Theme.of(context).textTheme.headline1,
+              );
+            }),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              final counter = Provider.of<Counter>(context, listen: false);
+              counter.increment();
+            },
+            child: const Icon(Icons.add),
+          ),
+        ));
   }
 }
